0221a9df4f922f01f86b1e5737ef983825423fee
chriss-mo <156863651+chriss-mo@users.noreply.github.com>
2024-04-06 11:59:31 -0700

    Add more pages

--- Patient-Match/src/App.jsx
+++ Patient-Match/src/App.jsx
@@ -1,17 +1,19 @@
 // App.js
 
 import React from "react";
-import { BrowserRouter as Router, Route } from "react-router-dom";
-import Home from "./components/Home.jsx";
-import Registration from "./components/Registration.jsx";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Home from "./components/Home";
+import Registration from "./components/Registration";
+import ChartDrop from "./components/ChartDrop";
 
 function App() {
   return (
     <Router>
-      <div>
-        <Route path="/" exact component={Home} />
-        <Route path="/second" component={SecondPage} />
-      </div>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/Reg" element={<Registration />} />
+        <Route path="/ChartDrop" element={<ChartDrop />} />
+      </Routes>
     </Router>
   );
 }
---
+++ Patient-Match/src/components/ChartDrop.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+function ChartDrop() {
+  return <h1>Upload your chart here:</h1>;
+}
+
+export default ChartDrop;

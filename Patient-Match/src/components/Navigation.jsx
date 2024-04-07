fc3b2beeae546d87886f2fe78baddeaa89cc4170
chriss-mo <156863651+chriss-mo@users.noreply.github.com>
2024-04-06 18:48:52 -0700

    Update Navigation.jsx

--- Patient-Match/src/components/Navigation.jsx
+++ Patient-Match/src/components/Navigation.jsx
@@ -3,7 +3,7 @@ import React from "react";
 import { Link } from "react-router-dom";
 import "./Navigation.css"; // Import CSS file for additional styling
 
-function Navigation() {
+const Navigation = () => {
   return (
     <nav className="nav-container">
       <ul className="nav-list">
@@ -25,6 +25,6 @@ function Navigation() {
       </ul>
     </nav>
   );
-}
+};
 
 export default Navigation;

ed11994c9fc8a9f1d47b81f3e2dfe0b298461132
chriss-mo <156863651+chriss-mo@users.noreply.github.com>
2024-04-06 17:11:45 -0700

    Nav bar :((

--- Patient-Match/src/App.jsx
+++ Patient-Match/src/App.jsx
@@ -1,24 +1,24 @@
 // App.js
-
 import React from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Home from "./components/Home";
 import Registration from "./components/Registration";
 import ChartDrop from "./components/ChartDrop";
 import Navigation from "./components/Navigation";
+import Nav_FB from "./components/Nav_FB";
 import About from "./components/About";
 import Host from "./components/Host";
 
 function App() {
   return (
     <Router>
-      <Navigation />
+      <Nav_FB />
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/Reg" element={<Registration />} />
         <Route path="/ChartDrop" element={<ChartDrop />} />
-        <Route path="/About" component={<About />} />
-        <Route path="/Host" component={<Host />} />
+        <Route path="/About" element={<About />} />
+        <Route path="/Host" element={<Host />} />
       </Routes>
     </Router>
   );
---
+++ Patient-Match/src/components/Nav_FB.jsx
@@ -0,0 +1,39 @@
+import Link from "next/link";
+import {
+  Navbar,
+  NavbarBrand,
+  NavbarCollapse,
+  NavbarLink,
+  NavbarToggle,
+} from "flowbite-react";
+
+function Nav_FB() {
+  return (
+    <Navbar fluid rounded>
+      <NavbarBrand as={Link} href="https://flowbite-react.com">
+        <img
+          src="/favicon.svg"
+          className="mr-3 h-6 sm:h-9"
+          alt="Flowbite React Logo"
+        />
+        <span className="self-center whitespace-nowrap text-xl font-semibold dark:text-white">
+          Flowbite React
+        </span>
+      </NavbarBrand>
+      <NavbarToggle />
+      <NavbarCollapse>
+        <NavbarLink href="#" active>
+          Home
+        </NavbarLink>
+        <NavbarLink as={Link} href="#">
+          About
+        </NavbarLink>
+        <NavbarLink href="#">Services</NavbarLink>
+        <NavbarLink href="#">Pricing</NavbarLink>
+        <NavbarLink href="#">Contact</NavbarLink>
+      </NavbarCollapse>
+    </Navbar>
+  );
+}
+
+export default Nav_FB;
